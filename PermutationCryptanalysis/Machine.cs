ba4ff5ce4ef2c1dab6478592884f2f36af054f73
Max Bondarchuk <bondarchuk.m.y@gmail.com>
2020-02-17 00:37:22 +0200

    Initial Machine generation

--- PermutationCryptanalysis/Machine.cs
+++ PermutationCryptanalysis/Machine.cs
@@ -1,7 +1,28 @@
-﻿namespace PermutationCryptanalysis
+﻿using System.Collections.Generic;
+
+namespace PermutationCryptanalysis
 {
 	public class Machine
 	{
+		private readonly int _m;
+		private readonly int _n;
 		
+		private readonly List<List<int>> _stateMatrix;
+
+		private readonly List<List<int>> _outputMatrix;
+
+		public Machine(int m, int n)
+		{
+			_m = m;
+			_n = n;
+
+			for (var i = 0; i < _m; i++)
+			{
+				for (var j = 0; j < _n; j++)
+				{
+					
+				}
+			}
+		}
 	}
 }
